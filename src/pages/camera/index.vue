9b7281f7ba954b2945bf482b6ed84813246add41
yuhaocan <haocan.yu@huijie-inc.com>
2019-07-05 18:35:59 +0800

    。。。。

---
+++ src/pages/camera/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  mounted() {
+    var constraints = {
+      audio: true,
+      video: true
+    };
+    window.navigator.mediaDevices.webkitGetUserMedia(constraints).then(
+      () => {
+        console.log("success");
+      },
+      () => {
+        console.log("fail");
+      }
+    );
+  }
+};
+</script>
