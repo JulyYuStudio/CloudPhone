5084aa6c9d495ca39d1f6e80a9ac9c2fba8e2f5d
yuhaocan <haocan.yu@huijie-inc.com>
2019-07-09 18:51:56 +0800

    do:demo

---
+++ src/components/demo_slot.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <div>
+      <slot name="header"></slot>
+    </div>
+    <div>
+      <slot></slot>
+    </div>
+    <div>
+      <slot name="footer"></slot>
+    </div>
+  </div>
+</template>
---
+++ src/pages/demo/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <demo>
+      <template v-slot:header>
+        <h1>Here might be a page title</h1>
+      </template>
+
+      <p>A paragraph for the main content.</p>
+      <p>And another one.</p>
+
+      <template v-slot:footer>
+        <p>Here's some contact info</p>
+      </template>
+    </demo>
+  </div>
+</template>
+
+<script>
+import demo from "~/components/demo_slot";
+export default {
+  components: {
+    demo
+  }
+};
+</script>
